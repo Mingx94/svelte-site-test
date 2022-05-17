d11ff708b2e60fcd2404e6efdafaa07ed7dc67f5
Michael Tsai <mingxcv@gmail.com>
2022-05-17 08:36:26 +0000

    Initialize

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<sevlte:head>
+    <title>Home</title>
+</sevlte:head>
+
+<script lang="ts">
+    let count = 0;
+
+    function increment() {
+        count++;
+    }
+</script>
+
+<p>{count}</p>
+<button on:click={increment}>Inc</button>
